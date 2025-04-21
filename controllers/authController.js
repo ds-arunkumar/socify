3d7b463212bb64ef2bb074c93227db14a480c7d2
ds-arunkumar <arunkumar519@gmail.com>
2025-04-21 22:31:43 +0530

    switched to bearer token

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,16 +56,8 @@ const authController = {
       // generate a token
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
-      // set the token in a cookie
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 3600000,
-      });
-
       // return a success message
-      res.status(200).json({ message: "User logged in successfully" });
+      res.status(200).json({ message: "User logged in successfully", token });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,8 +5,8 @@ const User = require("../models/user");
 const auth = {
   // middleware to check if the user is authenticated
   isAuthenticated: async (req, res, next) => {
-    // get the token from the cookie
-    const token = req.cookies.token;
+    // get the token from the authorization header
+    const token = req.headers.authorization.split(" ")[1];
 
     // send an error if the token is not present
     if (!token) {
